72b429fbc9bbc5e72388829866682347c178a4d2
High-Tech0823 <laiguoqiang34@gmail.com>
2024-08-03 19:31:44 +1000

    authprovider completed

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,16 @@
+import EventCard from "../components/EventCard";
 import Navbar from "../components/Navbar";
 
 const Main = () => {
     return (
         <div>
             <Navbar />
-            <div className="text-black">This is main page</div>
+            <div className="flex gap-5 justify-center">
+                <EventCard text="Science Beta" btn_text="View" onClick={() => { }} className="bg-red-300" img_url="" />
+                <EventCard text="Science Beta" btn_text="View" onClick={() => { }} className="bg-red-300" img_url="" />
+                <EventCard text="Science Beta" btn_text="View" onClick={() => { }} className="bg-red-300" img_url="" />
+                <EventCard text="Science Beta" btn_text="View" onClick={() => { }} className="bg-red-300" img_url="" />
+            </div>
         </div>
     )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
- export interface IconProps {
+
+export type KeyedObject = {
+    [key: string]: string | number | KeyedObject | any;
+  };
+  
+export interface IconProps {
     className?: string;
     size?: number;
     color?: string;
@@ -8,3 +13,42 @@ export interface ModalProps {
     onClose: () => void;
     title: string;
 }
+
+export interface EventCardProps {
+    text: string,
+    btn_text: string,
+    onClick: () => void,
+    img_url?: string,
+    className?: string,
+}
+
+// Auth types
+export type UserProfile = {
+    id?: string,
+    email?: string,
+    avatar?: string,
+    name?: string,
+    role?: string,
+}
+export interface AuthProps {
+    isLoggedIn: boolean,
+    isInitialized?: boolean,
+    user?: UserProfile | null,
+    token?: string | null
+}
+
+export interface AuthActionProps {
+    type: string,
+    payload?: AuthProps,
+}
+
+export type JWTContextType = {
+    isLoggedIn: boolean;
+    isInitialized?: boolean;
+    user?: UserProfile | null | undefined;
+    logout: () => void;
+    login: (email: string, password: string) => Promise<void>;
+    register: (email: string, password: string, firstName: string, lastName: string) => Promise<void>;
+    resetPassword: (email: string) => Promise<void>;
+    updateProfile: VoidFunction;
+  };
